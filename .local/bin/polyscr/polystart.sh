1bd0ef9f4f4ae59a17f544021f2561ea0ed2bab7
cs <cspencernd@gmail.com>
2021-07-25 07:43:45 -0700

    dmenuuincode

---
+++ .local/bin/polyscr/polystart.sh
@@ -0,0 +1,10 @@
+#!/bin/zsh
+
+killall -q polybar
+
+while pgrep -u $UID -x polybar >/dev/null; do
+    sleep 1;
+done
+
+polybar example -r & disown
+# polybar example -r 2>&1 | tee -a /tmp/polybar1.log & disown
