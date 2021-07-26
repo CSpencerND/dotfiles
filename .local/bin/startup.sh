7ab5f6b0388c7359a285cac028f285cb34b95ce1
cs <cspencernd@gmail.com>
2021-07-26 05:09:29 -0700

    autostart stuff

--- .local/bin/startup.sh
+++ .local/bin/startup.sh
@@ -1,5 +1,7 @@
 #!/bin/zsh
+# pactl load-module module-alsa-sink device=hw:1,1
+copyq & disown
+polybar example -r & disown
 system76-power charge-thresholds --profile max_lifespan
 sudo ifconfig enp35s0 down
-# pactl load-module module-alsa-sink device=hw:1,1
-# polystart.sh
+
