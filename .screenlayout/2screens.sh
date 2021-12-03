9473d0ad5cc7a3b2da5824f8f46a53637acfdeee
cs <cspencernd@gmail.com>
2021-12-02 21:18:07 -0500

    screens: 1280x720

--- .screenlayout/2screens.sh
+++ .screenlayout/2screens.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output eDP1 --mode 1920x1080 --pos 0x1080 --rotate normal --output DP1 --off --output DP2 --off --output DP3 --off --output DP4 --off --output HDMI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off
+xrandr --output eDP1 --mode 1280x720 --pos 0x1080 --rotate normal --output DP1 --off --output DP2 --off --output DP3 --off --output DP4 --off --output HDMI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off
