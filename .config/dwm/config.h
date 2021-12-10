84f8f72d883ad7cf91176e2ea2908578e1cc4d4f
cs <cspencernd@gmail.com>
2021-12-10 07:39:12 -0500

    dwm: vanity-gaps

--- .config/dwm/config.h
+++ .config/dwm/config.h
@@ -1,27 +1,31 @@
 // #include "./themes/dracula.h"   /* Importing Theme */
 
 /* appearance */
-static const unsigned int borderpx  = 2;        /* border pixel of windows */
-static const unsigned int gappx     = 12;        /* gaps between windows */
+static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int gappih    = 12;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 12;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 12;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 12;       /* vert outer gap between windows and screen edge */
+static int smartgaps          = 0;              /* 1 means no outer gap when there is only one window */
+static const int showbar            = 1;        /* 0 means no bar */
+static const int topbar             = 1;        /* 0 means bottom bar */
 
+static const int showsystray             = 0;     /* 0 means no systray */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayspacing = 4;   /* systray spacing */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
-static const int showsystray        = 1;     /* 0 means no systray */
-static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
 
 static const Bool viewontag         = False;     /* Switch view on tag switch */
 
 #define ICONSIZE 22   /* icon size */
 #define ICONSPACING 2 /* space between icon and title */
 
-static const char *fonts[] = { 
-        "Weather Icons:pixelsize=14:antialias=true:autohint=true",
-        "Hack Nerd Font:size=10",
-        "JoyPixels:pixelsize=10:antialias=true:autohint=true"
-        "monospace:size=10" 
+static const char *fonts[] = {
+	"Weather Icons:pixelsize=14:antialias=true:autohint=true",
+	"Hack Nerd Font:size=10",
+	"JoyPixels:pixelsize=10:antialias=true:autohint=true"
+	"monospace:size=10"
 };
 static const char dmenufont[]       = "Hack Nerd Font:size=10";
 
@@ -33,45 +37,45 @@ static const char bg_sel[]          = "#bd93f9";
 static const char bor_sel[]         = "#bd93f9";
 
 static const char *colors[][3]      = {
-    /*               fg         bg         border   */
-    [SchemeNorm] = { fg_norm, bg_norm, bor_norm },
-    [SchemeSel]  = { fg_sel, bg_sel,  bor_sel },
-    [SchemeHid]  = { bor_sel,  bg_norm, bor_sel },
+	/*               fg         bg         border   */
+	[SchemeNorm] = { fg_norm, bg_norm, bor_norm },
+	[SchemeSel]  = { fg_sel, bg_sel,  bor_sel },
+	[SchemeHid]  = { bor_sel,  bg_norm, bor_sel },
 };
 static const unsigned int baralpha = 0xcc;
 static const unsigned int borderalpha = OPAQUE;
 
 static const unsigned int alphas[][3] = {
-    /*               fg      bg        border     */
-    [SchemeNorm] = { OPAQUE, baralpha, borderalpha },
-    [SchemeSel]  = { OPAQUE, baralpha, borderalpha },
+	/*               fg      bg        border     */
+	[SchemeNorm] = { OPAQUE, baralpha, borderalpha },
+	[SchemeSel]  = { OPAQUE, baralpha, borderalpha },
 };
 
 /* scratchpad */
 typedef struct {
-    const char *name;
-    const void *cmd;
+	const char *name;
+	const void *cmd;
 } Sp;
 const char *spcmd1[] = { "kitty", "-e", "btop", NULL };
 static Sp scratchpads[] = {
-    /* name     cmd  */
-    {"sptop",   spcmd1},
+	/* name     cmd  */
+	{"sptop",   spcmd1},
 };
 
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5" };
 
 static const Rule rules[] = {
-    /* xprop(1):
-     *	WM_CLASS(STRING) = instance, class
-     *	WM_NAME(STRING) = title
-     *  use tags mask to point an application to a specific workspace
-     */
-    /* class                instance    title       tags mask   isfloating   monitor */
-    { "Gimp",               NULL,       NULL,       0,          0,           -1 },
-    { "Xfce4-terminal",     NULL,       NULL,       0,          1,           -1 },
-    { "firefox",            NULL,       NULL,       0,          0,           -1 },
-    { NULL,                 "sptop",	NULL,	    SPTAG(0),   1,           -1 },
+	/* xprop(1):
+	 *	WM_CLASS(STRING) = instance, class
+	 *	WM_NAME(STRING) = title
+	 *  use tags mask to point an application to a specific workspace
+	 */
+	/* class                instance    title       tags mask   isfloating   monitor */
+	{ "Gimp",               NULL,       NULL,       0,          0,           -1 },
+	{ "Xfce4-terminal",     NULL,       NULL,       0,          1,           -1 },
+	{ "firefox",            NULL,       NULL,       0,          0,           -1 },
+	{ NULL,                 "sptop",    NULL,       SPTAG(0),   1,           -1 },
 };
 
 /* layout(s) */
@@ -79,34 +83,38 @@ static const float mfact     = 0.50; /* factor of master area size [0.05..0.95] 
 static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 
-#include "layouts.c"
-#include "tatami.c"
+#define FORCE_VSPLIT 1  /* nrowgrid layout: force two clients to always split vertically */
+// #include "tatami.c"
+#include "vanitygaps.c"
+
 
 static const Layout layouts[] = {
-    /* symbol     arrange function */
-    { "[]=",      tile },    /* first entry is default */
-    { "HHH",      grid },
-    { "[M]",      monocle },
-    { "|+|",      tatami },
-    // { "H[]",      deck },
-    // { "TTT",      bstack },
-    // { "===",      bstackhoriz},
-    // { "###",      nrowgrid },
-    // { "---",      horizgrid },
-    // { ":::",      gaplessgrid },
-    { "|M|",      centeredmaster },
-    { ">M>",      centeredfloatingmaster },
-    { "><>",      NULL },    /* no layout function means floating behavior */
-    { NULL,       NULL },
+	/* symbol     arrange function */
+	{ "[]=",      tile },/* first entry is default */
+	{ "[M]",      monocle },
+	{ "|+|",      tatami },
+	{ "[@]",      spiral },
+	{ "[\\]",     dwindle },
+	{ "H[]",      deck },
+	{ "TTT",      bstack },
+	{ "===",      bstackhoriz},
+	{ "HHH",      grid },
+	{ "###",      nrowgrid },
+	{ "---",      horizgrid },
+	{ ":::",      gaplessgrid },
+	{ "|M|",      centeredmaster },
+	{ ">M>",      centeredfloatingmaster },
+	{ "><>",      NULL },/* no layout function means floating behavior */
+	{ NULL,       NULL },
 };
 
 /* key definitions */
 #define MODKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
-    { MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
-    { MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
-    { MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
-    { MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
+	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
+	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
+	{ MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
+	{ MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
 
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
@@ -124,88 +132,97 @@ static const char *taskmanager[]  = { "xfce4-taskmanager", NULL };
 #include "shift-tools-scratchpads.c"
 
 static Key keys[] = {
-    /* modifier             key                 function        argument */
+	/* modifier             key                 function        argument */
 
-    // window move
-    { MODKEY,               XK_j,               focusstackvis,  {.i = +1 } },
-    { MODKEY,               XK_k,               focusstackvis,  {.i = -1 } },
-    { MODKEY|ControlMask,   XK_j,               focusstackhid,  {.i = +1 } },
-    { MODKEY|ControlMask,   XK_k,               focusstackhid,  {.i = -1 } },
-    { MODKEY|ShiftMask,     XK_j,               rotatestack,    {.i = +1} },
-    { MODKEY|ShiftMask,     XK_k,               rotatestack,    {.i = -1} },
+	// window move
+	{ MODKEY,               XK_j,               focusstackvis,  {.i = +1 } },
+	{ MODKEY,               XK_k,               focusstackvis,  {.i = -1 } },
+	{ MODKEY|ControlMask,   XK_j,               focusstackhid,  {.i = +1 } },
+	{ MODKEY|ControlMask,   XK_k,               focusstackhid,  {.i = -1 } },
+	{ MODKEY|ShiftMask,     XK_j,               rotatestack,    {.i = +1} },
+	{ MODKEY|ShiftMask,     XK_k,               rotatestack,    {.i = -1} },
 
-    // window size
-    { MODKEY,               XK_h,               setmfact,       {.f = -0.05} },
-    { MODKEY,               XK_l,               setmfact,       {.f = +0.05} },
+	// window size
+	{ MODKEY,               XK_h,               setmfact,       {.f = -0.05} },
+	{ MODKEY,               XK_l,               setmfact,       {.f = +0.05} },
 
-    // window control
-    { MODKEY,               XK_m,               hide,           {0} },
-    { MODKEY|ControlMask,   XK_m,               show,           {0} },
-    { MODKEY|ShiftMask,     XK_m,               togglefullscr,  {0} },
-    { MODKEY|Mod1Mask,      XK_m,               setlayout,      {.v = &layouts[2]} },
-    { MODKEY,               XK_f,               togglefloating, {0} },
-    { MODKEY,               XK_c,               killclient,     {0} },
+	// window control
+	{ MODKEY,               XK_m,               hide,           {0} },
+	{ MODKEY|ControlMask,   XK_m,               show,           {0} },
+	{ MODKEY|ShiftMask,     XK_m,               togglefullscr,  {0} },
+	{ MODKEY|Mod1Mask,      XK_m,               setlayout,      {.v = &layouts[2]} },
+	{ MODKEY,               XK_f,               togglefloating, {0} },
+	{ MODKEY,               XK_c,               killclient,     {0} },
 
-    // shift-tools
-    { MODKEY,               XK_Tab,             shiftview,      {.i = +1 } },
-    { MODKEY|ShiftMask,     XK_Tab,             shiftview,      {.i = -1 } },
-    { MODKEY,               XK_i,               shiftview,      {.i = +1 } },
-    { MODKEY,               XK_u,               shiftview,      {.i = -1 } },
-    { MODKEY|ShiftMask,     XK_i,               shifttag,       {.i = +1 } },
-    { MODKEY|ShiftMask,	    XK_u,               shifttag,       {.i = -1 } },
-    { MODKEY|ControlMask,   XK_i,               shiftboth,      {.i = +1 } },
-    { MODKEY|ControlMask,   XK_u,               shiftboth,      {.i = -1 } },
-    { MODKEY|Mod1Mask,      XK_i,               shiftswaptags,  {.i = +1 } },
-    { MODKEY|Mod1Mask,	    XK_u,               shiftswaptags,  {.i = -1 } },
+	// shift-tools
+	{ MODKEY,               XK_Tab,             shiftview,      {.i = +1 } },
+	{ MODKEY|ShiftMask,     XK_Tab,             shiftview,      {.i = -1 } },
+	{ MODKEY,               XK_i,               shiftview,      {.i = +1 } },
+	{ MODKEY,               XK_u,               shiftview,      {.i = -1 } },
+	{ MODKEY|ShiftMask,     XK_i,               shifttag,       {.i = +1 } },
+	{ MODKEY|ShiftMask,     XK_u,               shifttag,       {.i = -1 } },
+	{ MODKEY|ControlMask,   XK_i,               shiftboth,      {.i = +1 } },
+	{ MODKEY|ControlMask,   XK_u,               shiftboth,      {.i = -1 } },
+	{ MODKEY|Mod1Mask,      XK_i,               shiftswaptags,  {.i = +1 } },
+	{ MODKEY|Mod1Mask,      XK_u,               shiftswaptags,  {.i = -1 } },
 
-    // screen
-    { MODKEY,               XK_comma,           focusmon,       {.i = -1 } },
-    { MODKEY,               XK_period,          focusmon,       {.i = +1 } },
-    { MODKEY|ShiftMask,     XK_comma,           tagmon,         {.i = -1 } },
-    { MODKEY|ShiftMask,     XK_period,          tagmon,         {.i = +1 } },
-    { MODKEY|ControlMask,   XK_comma,           travelmon,      {.i = -1 } },
-    { MODKEY|ControlMask,   XK_period,          travelmon,      {.i = +1 } },
+	// screen
+	{ MODKEY,               XK_comma,           focusmon,       {.i = -1 } },
+	{ MODKEY,               XK_period,          focusmon,       {.i = +1 } },
+	{ MODKEY|ShiftMask,     XK_comma,           tagmon,         {.i = -1 } },
+	{ MODKEY|ShiftMask,     XK_period,          tagmon,         {.i = +1 } },
+	{ MODKEY|ControlMask,   XK_comma,           travelmon,      {.i = -1 } },
+	{ MODKEY|ControlMask,   XK_period,          travelmon,      {.i = +1 } },
 
-    // layout
-    { MODKEY,               XK_bracketleft,     cyclelayout,    {.i = -1 } },
-    { MODKEY,               XK_bracketright,    cyclelayout,    {.i = +1 } },
+	// layout
+	{ MODKEY,               XK_bracketleft,     cyclelayout,    {.i = -1 } },
+	{ MODKEY,               XK_bracketright,    cyclelayout,    {.i = +1 } },
 
-    // scratchpad
-    { MODKEY,               XK_t,               togglescratch,  {.ui = 0 } },
+	// scratchpad
+	{ MODKEY,               XK_t,               togglescratch,  {.ui = 0 } },
 
-    // misc
-    { MODKEY|ShiftMask,     XK_b,               togglebar,      {0} },
-    { MODKEY,               XK_equal,           incnmaster,     {.i = +1 } },
-    { MODKEY,               XK_minus,           incnmaster,     {.i = -1 } },
+	// misc
+	{ MODKEY|ShiftMask,     XK_b,               togglebar,      {0} },
+	{ MODKEY,               XK_equal,           incnmaster,     {.i = +1 } },
+	{ MODKEY,               XK_minus,           incnmaster,     {.i = -1 } },
 
-    // quit / restart
-    { MODKEY,               XK_r,               self_restart,   {0} },
-    { MODKEY|ShiftMask,     XK_x,               quit,           {0} },
+	// quit / restart
+	{ MODKEY,               XK_r,               self_restart,   {0} },
+	{ MODKEY|ShiftMask,     XK_x,               quit,           {0} },
 
-    // ????
-    { MODKEY,               XK_semicolon,       view,           {0} },
-    { MODKEY|ShiftMask,     XK_semicolon,       view,           {.ui = ~0 } },
-    { MODKEY,               XK_apostrophe,      zoom,           {0} },
-    { MODKEY|ShiftMask,     XK_apostrophe,      tag,            {.ui = ~0 } },
+	// ????
+	{ MODKEY,               XK_semicolon,       view,           {0} },
+	{ MODKEY|ShiftMask,     XK_semicolon,       view,           {.ui = ~0 } },
+	{ MODKEY,               XK_apostrophe,      zoom,           {0} },
+	{ MODKEY|ShiftMask,     XK_apostrophe,      tag,            {.ui = ~0 } },
 
-    TAGKEYS(XK_1, 0)
-    TAGKEYS(XK_2, 1)
-    TAGKEYS(XK_3, 2)
-    TAGKEYS(XK_4, 3)
-    TAGKEYS(XK_5, 4)
-    TAGKEYS(XK_6, 5)
+	// gaps
+	{ MODKEY,               XK_a,      incrgaps,       {.i = +1 } },
+	{ MODKEY,               XK_z,      incrgaps,       {.i = -1 } },
+	{ MODKEY|ShiftMask,     XK_a,      togglegaps,     {0} },
+	{ MODKEY|ShiftMask,     XK_z,      defaultgaps,    {0} },
 
-    // unused
-    // { MODKEY,            XK_t,       setlayout,      {.v = &layouts[0]} },
-    // { MODKEY,            XK_g,       setlayout,      {.v = &layouts[1]} },
-    // { MODKEY,            XK_m,       setlayout,      {.v = &layouts[2]} },
-    // { MODKEY,            XK_f,       setlayout,      {.v = &layouts[2]} },
+	TAGKEYS(XK_1, 0)
+	TAGKEYS(XK_2, 1)
+	TAGKEYS(XK_3, 2)
+	TAGKEYS(XK_4, 3)
+	TAGKEYS(XK_5, 4)
+	TAGKEYS(XK_6, 5)
+
+	// unused
+	// { MODKEY,            XK_t,       setlayout,      {.v = &layouts[0]} },
+	// { MODKEY,            XK_g,       setlayout,      {.v = &layouts[1]} },
+	// { MODKEY,            XK_m,       setlayout,      {.v = &layouts[2]} },
+	// { MODKEY,            XK_f,       setlayout,      {.v = &layouts[2]} },
 };
 
 /* button definitions */
 /* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
 static Button buttons[] = {
 	/* click                event mask      button          function        argument */
+	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
+	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
+	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
 	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
 	{ ClkLtSymbol,          0,              Button2,        setlayout,      {.v = &layouts[2]} },
 	{ ClkWinTitle,          0,              Button1,        togglewin,      {0} },
@@ -213,9 +230,6 @@ static Button buttons[] = {
 	{ ClkStatusText,        0,              Button1,        spawn,          {.v = taskmanager } },
 	{ ClkStatusText,        0,              Button2,        spawn,          {.v = filecmd } },
 	{ ClkStatusText,        0,              Button3,        spawn,          {.v = calendar } },
-	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
-	{ ClkClientWin,         MODKEY,         Button3,        togglefloating, {0} },
-	{ ClkClientWin,         MODKEY,         Button2,        resizemouse,    {0} },
 	{ ClkTagBar,            0,              Button1,        view,           {0} },
 	{ ClkTagBar,            0,              Button2,        toggleview,     {0} },
 	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
--- .config/dwm/dwm.c
+++ .config/dwm/dwm.c
@@ -150,7 +150,10 @@ struct Monitor {
 	int bt;               /* number of tasks */
 	int mx, my, mw, mh;   /* screen size */
 	int wx, wy, ww, wh;   /* window area  */
-	int gappx;            /* gaps between windows */
+	int gappih;           /* horizontal gap between windows */
+	int gappiv;           /* vertical gap between windows */
+	int gappoh;           /* horizontal outer gaps */
+	int gappov;           /* vertical outer gaps */
 	unsigned int seltags;
 	unsigned int sellt;
 	unsigned int tagset[2];
@@ -267,7 +270,6 @@ static void spawn(const Arg *arg);
 static Monitor *systraytomon(Monitor *m);
 static void tag(const Arg *arg);
 static void tagmon(const Arg *arg);
-static void tile(Monitor *);
 static void togglebar(const Arg *arg);
 static void togglefloating(const Arg *arg);
 static void togglescratch(const Arg *arg);
@@ -303,8 +305,6 @@ static int xerrordummy(Display *dpy, XErrorEvent *ee);
 static int xerrorstart(Display *dpy, XErrorEvent *ee);
 static void xinitvisual();
 static void zoom(const Arg *arg);
-static void centeredmaster(Monitor *m);
-static void centeredfloatingmaster(Monitor *m);
 static void travelmon(const Arg *arg);
 
 /* variables */
@@ -795,7 +795,10 @@ createmon(void)
 	m->nmaster = nmaster;
 	m->showbar = showbar;
 	m->topbar = topbar;
-	m->gappx = gappx;
+	m->gappih = gappih;
+	m->gappiv = gappiv;
+	m->gappoh = gappoh;
+	m->gappov = gappov;
 	m->lt[0] = &layouts[0];
 	m->lt[1] = &layouts[1 % LENGTH(layouts)];
 	strncpy(m->ltsymbol, layouts[0].symbol, sizeof m->ltsymbol);
@@ -2179,16 +2182,6 @@ setfullscreen(Client *c, int fullscreen)
 	}
 }
 
-// void
-// setgaps(const Arg *arg)
-// {
-// 	if ((arg->i == 0) || (selmon->gappx + arg->i < 0))
-// 		selmon->gappx = 0;
-// 	else
-// 		selmon->gappx += arg->i;
-// 	arrange(selmon);
-// }
-
 void
 setlayout(const Arg *arg)
 {
@@ -2398,32 +2391,6 @@ tagmon(const Arg *arg)
 	if (!selmon->sel || !mons->next)
 		return;
 	sendmon(selmon->sel, dirtomon(arg->i));
-}
-
-void
-tile(Monitor *m)
-{
-	unsigned int i, n, h, mw, my, ty;
-	Client *c;
-
-	for (n = 0, c = nexttiled(m->clients); c; c = nexttiled(c->next), n++);
-	if (n == 0)
-		return;
-
-	if (n > m->nmaster)
-		mw = m->nmaster ? m->ww * m->mfact : 0;
-	else
-		mw = m->ww - m->gappx;
-	for (i = 0, my = ty = m->gappx, c = nexttiled(m->clients); c; c = nexttiled(c->next), i++)
-		if (i < m->nmaster) {
-			h = (m->wh - my) / (MIN(n, m->nmaster) - i) - m->gappx;
-			resize(c, m->wx + m->gappx, m->wy + my, mw - (2*c->bw) - m->gappx, h - (2*c->bw), 0);
-			my += HEIGHT(c) + m->gappx;
-		} else {
-			h = (m->wh - ty) / (n - i) - m->gappx;
-			resize(c, m->wx + mw + m->gappx, m->wy + ty, m->ww - mw - (2*c->bw) - 2*m->gappx, h - (2*c->bw), 0);
-			ty += HEIGHT(c) + m->gappx;
-		}
 }
 
 void
@@ -3146,6 +3113,13 @@ zoom(const Arg *arg)
 	pop(c);
 }
 
+void
+travelmon(const Arg *arg)
+{
+    tagmon(arg);
+    focusmon(arg);
+}
+
 int
 main(int argc, char *argv[])
 {
@@ -3171,112 +3145,3 @@ main(int argc, char *argv[])
 	return EXIT_SUCCESS;
 }
 
-void
-centeredmaster(Monitor *m)
-{
-	unsigned int i, n, h, mw, mx, my, oty, ety, tw;
-	Client *c;
-
-	/* count number of clients in the selected monitor */
-	for (n = 0, c = nexttiled(m->clients); c; c = nexttiled(c->next), n++);
-	if (n == 0)
-		return;
-
-	/* initialize areas */
-	mw = m->ww;
-	mx = 0;
-	my = 0;
-	tw = mw;
-
-	if (n > m->nmaster) {
-		/* go mfact box in the center if more than nmaster clients */
-		mw = m->nmaster ? m->ww * m->mfact : 0;
-		tw = m->ww - mw;
-
-		if (n - m->nmaster > 1) {
-			/* only one client */
-			mx = (m->ww - mw) / 2;
-			tw = (m->ww - mw) / 2;
-		}
-	}
-
-	oty = 0;
-	ety = 0;
-	for (i = 0, c = nexttiled(m->clients); c; c = nexttiled(c->next), i++)
-	if (i < m->nmaster) {
-		/* nmaster clients are stacked vertically, in the center
-		 * of the screen */
-		h = (m->wh - my) / (MIN(n, m->nmaster) - i);
-		resize(c, m->wx + mx, m->wy + my, mw - (2*c->bw),
-		       h - (2*c->bw), 0);
-		my += HEIGHT(c);
-	} else {
-		/* stack clients are stacked vertically */
-		if ((i - m->nmaster) % 2 ) {
-			h = (m->wh - ety) / ( (1 + n - i) / 2);
-			resize(c, m->wx, m->wy + ety, tw - (2*c->bw),
-			       h - (2*c->bw), 0);
-			ety += HEIGHT(c);
-		} else {
-			h = (m->wh - oty) / ((1 + n - i) / 2);
-			resize(c, m->wx + mx + mw, m->wy + oty,
-			       tw - (2*c->bw), h - (2*c->bw), 0);
-			oty += HEIGHT(c);
-		}
-	}
-}
-
-void
-centeredfloatingmaster(Monitor *m)
-{
-	unsigned int i, n, w, mh, mw, mx, mxo, my, myo, tx;
-	Client *c;
-
-	/* count number of clients in the selected monitor */
-	for (n = 0, c = nexttiled(m->clients); c; c = nexttiled(c->next), n++);
-	if (n == 0)
-		return;
-
-	/* initialize nmaster area */
-	if (n > m->nmaster) {
-		/* go mfact box in the center if more than nmaster clients */
-		if (m->ww > m->wh) {
-			mw = m->nmaster ? m->ww * m->mfact : 0;
-			mh = m->nmaster ? m->wh * 0.9 : 0;
-		} else {
-			mh = m->nmaster ? m->wh * m->mfact : 0;
-			mw = m->nmaster ? m->ww * 0.9 : 0;
-		}
-		mx = mxo = (m->ww - mw) / 2;
-		my = myo = (m->wh - mh) / 2;
-	} else {
-		/* go fullscreen if all clients are in the master area */
-		mh = m->wh;
-		mw = m->ww;
-		mx = mxo = 0;
-		my = myo = 0;
-	}
-
-	for(i = tx = 0, c = nexttiled(m->clients); c; c = nexttiled(c->next), i++)
-	if (i < m->nmaster) {
-		/* nmaster clients are stacked horizontally, in the center
-		 * of the screen */
-		w = (mw + mxo - mx) / (MIN(n, m->nmaster) - i);
-		resize(c, m->wx + mx, m->wy + my, w - (2*c->bw),
-		       mh - (2*c->bw), 0);
-		mx += WIDTH(c);
-	} else {
-		/* stack clients are stacked horizontally */
-		w = (m->ww - tx) / (n - i);
-		resize(c, m->wx + tx, m->wy, w - (2*c->bw),
-		       m->wh - (2*c->bw), 0);
-		tx += WIDTH(c);
-	}
-}
-
-void
-travelmon(const Arg *arg)
-{
-    tagmon(arg);
-    focusmon(arg);
-}
