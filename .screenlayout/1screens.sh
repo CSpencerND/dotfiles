fb1f415b430e56d30565d388372536e0d0b7b66d
cs <cspencernd@gmail.com>
2022-03-30 23:54:19 -0400

    monitor stuff

--- .screenlayout/1screens.sh
+++ .screenlayout/1screens.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
-# xrandr --output eDP1 --scale 0.85x0.85 --mode 1920x1080 --pos 0x1080 --rotate normal --output DP1 --off --output DP2 --off --output DP3 --off --output DP4 --off --output HDMI1 --off --output VIRTUAL1 --off
+
+xrandr --newmode "1920x1080_120.00" 369.50 1920 2080 2288 2656 1080 1083 1088 1160 -hsync +vsync && \
+xrandr --addmode eDP1 1920x1080_120.00 && \
+xrandr --output eDP1 --scale 0.667x0.667 --mode 1920x1080_120.00 --pos 0x1080 --rotate normal --output DP1 --off --output DP2 --off --output DP3 --off --output DP4 --off --output HDMI1 --off --output VIRTUAL1 --off || \
 xrandr --output eDP1 --scale 0.667x0.667 --mode 1920x1080 --pos 0x1080 --rotate normal --output DP1 --off --output DP2 --off --output DP3 --off --output DP4 --off --output HDMI1 --off --output VIRTUAL1 --off
-# xrandr --output eDP1 --mode 1280x720 --pos 0x1080 --rotate normal --output DP1 --off --output DP2 --off --output DP3 --off --output DP4 --off --output HDMI1 --off --output VIRTUAL1 --off
+
+# xrandr --newmode "1280x720_120.00" 162.00 1280 1376 1512 1744 720 723 728 775 -hsync +vsync && \
+# xrandr --addmode eDP1 1280x720_120.00 && \
+# xrandr --output eDP1 --scale 1x1 --mode 1280x720_120.00 --pos 0x1080 --rotate normal --output DP1 --off --output DP2 --off --output DP3 --off --output DP4 --off --output HDMI1 --off --output VIRTUAL1 --off || \
+# xrandr --output eDP1 --scale 1x1 --mode 1280x720 --pos 0x1080 --rotate normal --output DP1 --off --output DP2 --off --output DP3 --off --output DP4 --off --output HDMI1 --off --output VIRTUAL1 --off
--- .screenlayout/2screens.sh
+++ .screenlayout/2screens.sh
@@ -1,17 +1,10 @@
 #!/bin/sh
 
-### low dpi that linux can handle
-xrandr --output eDP1 --mode 1280x720 --pos 300x1080 --rotate normal --output DP1 --off --output DP2 --off --output DP3 --off --output DP4 --off --output HDMI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off
-# xrandr --output eDP1 --mode 1600x900 --pos 300x1080 --rotate normal --output DP1 --off --output DP2 --off --output DP3 --off --output DP4 --off --output HDMI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off
-
-### 1080
-# xrandr --output eDP1 --mode 1920x1080 --scale 1x1 --pos 320x1440 --output HDMI1 --primary --mode 1920x1080 --scale 1.25x1.25 --pos 0x0
-
-### 4k
-# xrandr --output eDP1 --mode 1920x1080 --scale 1x1 --pos 960x2160 --output HDMI1 --primary --mode 3840x2160 --scale .75x.75 --pos 0x0
+### 1k
+# xrandr --output eDP1 --mode 1280x720 --pos 300x1080 --rotate normal --output DP1 --off --output DP2 --off --output DP3 --off --output DP4 --off --output HDMI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off
 
 ### 2k (MAIN)
-# xrandr --output eDP1 --mode 1920x1080 --scale 0.825x0.825 --pos 320x1440 --output HDMI1 --primary --mode 2560x1440 --scale 1x1 --pos 0x0
+# xrandr --output eDP1 --scale 0.667x0.667 --mode 1920x1080 --pos 320x1440 --rotate normal --output DP1 --off --output DP2 --off --output DP3 --off --output DP4 --off --output HDMI1 --primary --scale 0.75x0.75 --mode 2560x1440 --pos 0x0 --rotate normal --output VIRTUAL1 --off
 
-### 2k Alt
-# xrandr --output eDP1 --mode 1920x1080 --scale 1x1 --pos 480x2160 --output HDMI1 --primary --mode 2560x1440 --scale 1.25x1.25 --pos 0x0
+### 4k
+xrandr --output eDP1 --scale 0.667x0.667 --mode 1920x1080 --pos 960x2160 --rotate normal --output DP1 --off --output DP2 --off --output DP3 --off --output DP4 --off --output HDMI1 --primary --scale 0.5x0.5 --mode 3840x2160 --pos 0x0 --rotate normal --output VIRTUAL1 --off
