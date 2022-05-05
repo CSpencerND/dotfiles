6bd1bdab6bb3cf06bd4c93c4bfe938ebaca8c7f2
cs <cspencernd@gmail.com>
2022-05-05 10:32:40 -0400

    new dwm build

---
+++ .local/share/dwm/patch/shiftview.h
@@ -0,0 +1,5 @@
+static void shiftview(const Arg *arg);
+static void shifttag(const Arg *arg);
+static void shiftboth(const Arg *arg);
+static void swaptags(const Arg *arg);
+static void shiftswaptags(const Arg *arg);
