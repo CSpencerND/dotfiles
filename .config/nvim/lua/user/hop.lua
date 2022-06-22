f882aa4a57f283f72136994009c1ce84558e14b8
cs <cspencernd@gmail.com>
2022-06-22 02:43:40 -0400

    fix hop

--- .config/nvim/lua/user/hop.lua
+++ .config/nvim/lua/user/hop.lua
@@ -10,7 +10,7 @@ local keymap = vim.api.nvim_set_keymap
 
 
 keymap("", "<A-s>", ":HopWordCurrentLine<cr>", { silent = true })
-keymap("", "S", ":HopChar2<cr>", { silent = true })
+keymap("", "S", ":HopWord<cr>", { silent = true })
 
 keymap("o", "f", ":lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true, inclusive_jump = true })<CR>", opts)
 keymap("o", "F", ":lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true })<CR>", opts)
