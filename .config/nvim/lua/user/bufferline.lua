f6039c5a5a0a7c73cb4110f8d49712cb57d3c2c1
cs <cspencernd@gmail.com>
2022-06-27 01:04:20 -0400

    rm close_button

--- .config/nvim/lua/user/bufferline.lua
+++ .config/nvim/lua/user/bufferline.lua
@@ -29,7 +29,7 @@ bufferline.setup {
         themable = true,
         show_buffer_icons = true,
         show_buffer_close_icons = true,
-        show_close_icon = true,
+        show_close_icon = false,
         show_tab_indicators = true,
         persist_buffer_sort = true, -- whether or not custom sorted buffers should persist
         separator_style = "thin" , -- { "▊", "▊" } | "thick" | "thin" | { 'any', 'any' },
