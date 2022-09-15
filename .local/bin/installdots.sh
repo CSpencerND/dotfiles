5bf7e4aaf842492f735b962893963a05f5004264
cs <cspencernd@gmail.com>
2022-09-14 23:55:34 -0400

    add installdots.sh

---
+++ .local/bin/installdots.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+echo "This script hasn't been tested. Use at your own risk."
+
+sudo pacman -Syu
+sudo pacman -S --needed paru-bin
+sudo paru -S --needed - <(curl -s https://raw.githubusercontent.com/CSpencerND/dotfiles/main/Documents/backup/pkgs/all-pkgs)
+
+chsh -s "$(which zsh)"
+
+alias backup='/usr/bin/git --git-dir=$HOME/.backup/ --work-tree=$HOME'
+git clone --bare git@github.com:CSpencerND/dotfiles.git "$HOME"/.backup
+
+mkdir -p .config-backup && \
+backup checkout 2>&1 | grep -E "\s+\." | awk {'print $1'} | \
+xargs -I{} mv {} .config-backup/{}
+
+backup checkout
+backup config --local status.showUntrackedFiles no
+
