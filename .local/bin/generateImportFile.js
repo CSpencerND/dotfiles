1ccb738827a8b53515981494fed5e68fdbdd0e7a
cspencer <cspencernd@gmail.com>
2023-01-06 07:10:51 -0500

    update to new format

--- .local/bin/generateImportFile.js
+++ .local/bin/generateImportFile.js
@@ -1,51 +1,75 @@
 #!/usr/bin/env node
 
 const fs = require("fs")
-const path = require("path")
-
 const cwd = process.cwd()
 
 let imports = ""
-const imageImportsTest = []
+const imageImports = []
 
-fs.readdirSync(cwd).forEach((file) => {
-    if (/(\.jpg|\.png|\.gif|\.webp|\.avif)$/.test(file)) {
-        const fileNameWithoutExtension = file
-            .replace(/\.[^/.]+$/, "")
-            .replace(/-/g, "")
-            .replace(/[^\w]/g, "")
-        imports += `import ${fileNameWithoutExtension} from "./${file}";\n`
-        imageImportsTest.push(
-            `[${fileNameWithoutExtension}, "${fileNameWithoutExtension}"]`
-        )
+fs.readdir(cwd, (err, files) => {
+    if (err) throw err
+
+    // Loop through each file
+    for (const file of files) {
+        // Check if the file extension matches the desired ones using a regular expression
+        if (/\.(jpg|png|gif|webp|avif)$/i.test(file)) {
+            // Get the file name without the extension
+            const fileNameWithoutExtension = file
+                .replace(/\.[^.]+$/, "")
+                .replace(/-/g, "")
+                .replace(/[^\w]/g, "")
+
+            // Add the import statement for the file
+            imports += `import ${fileNameWithoutExtension} from "./${file}";\n`
+
+            // Add the file to the imageImports array
+            imageImports.push(
+                `[${fileNameWithoutExtension}, "${fileNameWithoutExtension}"]`
+            )
+        }
     }
+
+    // Create the output string
+    const output = `import { ImageImport, ImageDataX } from "~/types";
+${imports}
+const imageImports: ImageImport[] = [
+    ${imageImports.join(", ")}
+]
+
+const imageImportsX: ImageDataX[] = []
+
+imageImports.forEach((imageImport) => {
+    const imageData = imageImport[0]
+    const alt: string = imageImport[1]
+
+    imageData.alt = alt
+    imageImportsX.push(imageData)
 })
 
-const output = `import { StaticImageData } from 'next/image';
-${imports}
-type ImageImport = [StaticImageData, string];
-const imageImportsTest: ImageImport[] = [
-  ${imageImportsTest.join(", ")}
-];
-export default imageImportsTest;`
+export default imageImportsX;`
 
-if (fs.existsSync("index.ts")) {
-    const answer = await new Promise((resolve) => {
-        process.stdin.resume()
-        process.stdin.setEncoding("utf8")
-        process.stdout.write(
-            "index.ts already exists. Do you want to overwrite it? (y/N) "
-        )
-        process.stdin.on("data", (text) => {
-            resolve(text)
-        })
+    // Check if the index.ts file already exists
+    fs.stat("index.ts", (error) => {
+        if (error) {
+            // If the file doesn't exist, create it and write the output to it
+            fs.writeFile("index.ts", output, (writeError) => {
+                if (writeError) throw writeError
+            })
+        } else {
+            // If the file exists, ask the user if they want to overwrite it
+            process.stdout.write(
+                "index.ts already exists. Do you want to overwrite it? (y/N) "
+            )
+            process.stdin.on("data", (data) => {
+                if (data.toString().trim().toLowerCase() === "y") {
+                    // If the user wants to overwrite it, write the output to the file
+                    fs.writeFile("index.ts", output, (writeError) => {
+                        if (writeError) throw writeError
+                    })
+                } else {
+                    console.log("Aborted")
+                }
+            })
+        }
     })
-
-    if (answer.toLowerCase() === "y") {
-        fs.writeFileSync("index.ts", output)
-    } else {
-        console.log("Aborted")
-    }
-} else {
-    fs.writeFileSync("index.ts", output)
-}
+})
