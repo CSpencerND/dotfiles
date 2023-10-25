b867718d92ed3325184fcd793c23a0ded07d6a18
cspencer <cspencernd@gmail.com>
2023-10-25 11:02:02 -0400

    add autocmd to disable autocomments on newlines

--- .config/nvim/lua/custom/init.lua
+++ .config/nvim/lua/custom/init.lua
@@ -3,39 +3,40 @@ vim.g.vscode_snippets_path = "/home/cs/.config/nvim/lua/custom/snippets/"
 -- Autocommands ------------------------------
 local autocmd = vim.api.nvim_create_autocmd
 
--- Auto resize panes when resizing nvim window
 autocmd("VimResized", {
     pattern = "*",
     command = "tabdo wincmd =",
+    desc = "Auto resize panes when resizing nvim window",
 })
 
--- exempt env files from lsp
 autocmd("BufEnter", {
     pattern = ".env*",
     group = vim.api.nvim_create_augroup("__env", { clear = true }),
     callback = function(args)
         vim.diagnostic.disable(args.buf)
     end,
+    desc = "Exempt env files from lsp",
 })
 
-local options = {
-    timeoutlen = 100, -- time to wait for a mapped sequence to complete (in milliseconds)
-    undofile = true, -- enable persistent undo
-    updatetime = 100, -- faster completion (4000ms default)
-    expandtab = true, -- convert tabs to spaces
-    shiftwidth = 4, -- the number of spaces inserted for each indentation
-    tabstop = 4, -- insert 2 spaces for a tab
-    cursorline = true, -- highlight the current line
-    number = true, -- set numbered lines
-    relativenumber = true, -- set relative numbered lines
-    wrap = false, -- display lines as one long line
-    title = true,
-    titlestring = "%F - neovim",
-    colorcolumn = "96",
-}
+autocmd("FileType", {
+    command = "set formatoptions-=cro",
+    desc = "Disable New Line Comment",
+})
 
-for k, v in pairs(options) do
-    vim.opt[k] = v
-end
+local opt = vim.opt
+
+opt.timeoutlen = 100 -- time to wait for a mapped sequence to complete (in milliseconds)
+opt.undofile = true -- enable persistent undo
+opt.updatetime = 100 -- faster completion (4000ms default)
+opt.expandtab = true -- convert tabs to spaces
+opt.shiftwidth = 4 -- the number of spaces inserted for each indentation
+opt.tabstop = 4 -- insert 2 spaces for a tab
+opt.cursorline = true -- highlight the current line
+opt.number = true -- set numbered lines
+opt.relativenumber = true -- set relative numbered lines
+opt.wrap = false -- display lines as one long line
+opt.title = true
+opt.titlestring = "%F - neovim"
+opt.colorcolumn = "96"
 
 require "custom.configs.lspsaga"
