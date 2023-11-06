133cc9393ef49a09198161a3f323736986680bd6
cspencer <cspencernd@gmail.com>
2023-11-06 01:44:27 -0500

    perf: update some stuff

--- .screenlayout/2screens.sh
+++ .screenlayout/2screens.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 xrandr --output eDP-1 --mode 1600x900 --pos 480x1440 --rotate normal \
-       --output DP-1 --primary --mode 2560x1440 --rate 75 --pos 1080x0 --rotate normal
+       --output DP-1 --primary --mode 2560x1440 --rate 75 --pos 1080x0 --rotate normal \
+       --output HDMI-1 --off --output DP-2 --off --output DP-3 --off --output DP-4 --off
